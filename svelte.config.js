0a9740e333d7b3e5a8be71ab33965f3b241967da
Kurt Pessa <kpessa@gmail.com>
2025-03-15 11:06:30 -0400

    working svelte 4 + tailwind + storyboook + shadcn-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+
+  // Add shadcn-svelte custom elements
+  kit: {
+    alias: {
+      $lib: './src/lib',
+      $components: './src/components'
+    }
+  }
 };
